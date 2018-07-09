1d0849f58919d5dfc5ad6b95a09537531d70b02f
Rowan Cota <rowan.cota@buzzfeed.com>
2018-07-09 18:00:02 -0400

    it ingests JSON theoretically

--- jsonconsumer.go
+++ jsonconsumer.go
@@ -1,8 +1,22 @@
 package main
 
-type JSONObject struct {
+import "encoding/json"
+
+type JSONLogObject struct {
+	date       string            `json:"date"`
+	date_day   string            `json:"date_day"`
+	date_month string            `json:"date_month"`
+	date_time  string            `json:"date_time"`
+	hostname   string            `json:"hostname"`
+	message    string            `json:"message"`
+	pid        string            `json:"pid"`
+	rig        map[string]string `json:"rig"`
+	syslog     map[string]string `json:"syslog"`
+	version    string            `json:"version"`
 }
 
-func (j *JSONObject) ingestJSON(json_blob) {
-
+func (j *JSONObject) ingestLogline(loglineString []byte) {
+	var formattedLog JSONLogObject
+	err := json.Unmarshal(loglineString, &formattedLog)
+	return formattedLog
 }
